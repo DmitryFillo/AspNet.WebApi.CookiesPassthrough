2273b4ae9b65c2d86fae4290d21e8419647414a0
DmitryFillo <fillo@fillo.me>
2018-10-11 20:50:43 +0300

    travis nunit runner exe filename fix + example project added

---
+++ AspNet.WebApi.CookiesPassthrough.Example/Controllers/CookiesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace AspNet.WebApi.CookiesPassthrough.Example.Controllers
+{
+    public class CookiesController : ApiController
+    {
+        [HttpGet]
+        [Route("examples/{id}")]
+        public IHttpActionResult GetExampleById([FromUri] int id)
+        {
+            var cookieDescriptors = new[]
+            {
+                new CookieDescriptor("test-cookie", "test-cookie"),
+                new CookieDescriptor("test-cookie-id", id.ToString()),
+            };
+
+            return Ok(new { id }).AddCookies(cookieDescriptors, Request.GetClientHost());
+        }
+    }
+}
