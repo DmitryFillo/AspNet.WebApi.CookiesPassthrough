a0518af66b10d6d2a91cb0906a5404ab416cc040
DmitryFillo <fillo@fillo.me>
2018-10-17 14:05:39 +0300

    deps fixes + nuspec upd

--- AspNet.WebApi.CookiesPassthrough.Example/Global.asax.cs
+++ AspNet.WebApi.CookiesPassthrough.Example/Global.asax.cs
@@ -1,8 +1,9 @@
-﻿using System.Web.Http;
+﻿using System.Web;
+using System.Web.Http;
 
 namespace AspNet.WebApi.CookiesPassthrough.Example
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
